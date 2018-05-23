6777a0ef3fdf3a586ba4f7ce7b046e73b3ab81c5
WadudAzad <azadmiu37@gmail.com>
2018-05-23 12:27:44 +0600

    add

---
+++ HTML 4/list/Order list.hs
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+	<head>
+		<title>Order list</title>
+		<meta charset = "uft-8"/>
+		<link rel="stylesheet" href="order.css" type="text/css" />
+
+	</head>
+	<body>
+		
+		<h1 id="ptitle">orderd list = ol tag <h1>
+		<ol class="abc">
+			<li>One </li>
+			<li>Two </li>
+			<li>Three </li>
+			<li>Four </li>
+			<li>Five </li>
+		</ol>
+		<ol type="i" class="abc">
+			<li>One </li>
+			<li>Two </li>
+			<li>Three </li>
+			<li>Four </li>
+			<li>Five </li>
+		</ol>
+		<ol type="A" class="abc">
+			<li>One </li>
+			<li>Two </li>
+			<li>Three </li>
+			<li>Four </li>
+			<li>Five </li>
+		</ol>
+		<ol type="a" class="abc">
+			<li>One </li>
+			<li>Two </li>
+			<li>Three </li>
+			<li>Four </li>
+			<li>Five </li>
+		</ol>
+		<h1 id="Unorder">unorderd list = ul tag <h1>
+		<ul>
+			<li>One </li>
+			<li>Two </li>
+			<li>Three </li>
+			<li>Four </li>
+			<li>Five </li>
+		</ul>
+	</body>
+</html>
